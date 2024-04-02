a63975107757d63bde85ae586a6484f3fa64538c
jeremy ruroede <jeremyruroede@gmail.com>
2024-04-02 13:07:00 -0400

    added first practice

---
+++ HackerRank/Basic_Data_Types.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <sstream>
+using namespace std;
+
+// declare funtion split(takes const reference to string named input)
+vector<string> split(const string& input ){
+    stringstream ss(input); //initialize input string as a stream
+    vector<string> tokens; // declare vector to store individual streams
+    string token;
+    //loop through string stream ss store all characters in token until ' '
+    while (getline(ss, token, ' ')) {
+        tokens.push_back(token); // append tokens vector with characters
+
+    }
+    return tokens;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string input; //= "3 12345678912345 a 334.23 14049.30493"
+    getline(cin, input); //get the input
+
+    vector<string> tokens = split(input); //split the input according to the function defined above
+
+    //iterate over each item in tokens, using auto to determine data type
+    for (const auto& token : tokens){
+        cout << token << endl; //print out
+
+    }
+
+    return 0;
+}
